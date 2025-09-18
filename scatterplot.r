45bc15299e08f38ccd355894764535665603e221
kkeerthi-19 <siri.keerthi19@gmail.com>
2025-09-18 23:27:46 +0530

    Update scatterplot.r

--- scatterplot.r
+++ scatterplot.r
@@ -1,6 +1,8 @@
-plot(mtcars$hp, mtcars$mpg,
-     main = "Scatter Plot of Horsepower vs MPG",
-     xlab = "Horsepower (hp)",
-     ylab = "Miles Per Gallon (MPG)",
-     pch = 20)
+# scatter plot 
+library(ggplot2) 
+ggplot(data = iris, aes(x = Petal.Length, y = Petal.Width)) + 
+geom_point(aes(color = Species)) + 
+labs(title = "Petal Length vs. Petal Width by Species", 
+x = "Petal Length", 
+y = "Petal Width")
 
