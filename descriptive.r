238b8525223195fca57a0fe8286c0bbf223420e1
kkeerthi-19 <siri.keerthi19@gmail.com>
2025-09-18 18:53:45 +0530

    Update descriptive.r

--- descriptive.r
+++ descriptive.r
@@ -1,7 +1,4 @@
-Data<-mtcars
-dim(Data)
-paste("Number of rows:",nrow(Data))
-paste("Number of columns:",ncol(Data))
-print("Summary")
-summary(Data)
+data(iris) 
+summary(iris) 
+summary(iris$Sepal.Length) 
 
