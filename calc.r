b838597476cbfe58902dc8e9083dfa282ca2adec
kkeerthi-19 <siri.keerthi19@gmail.com>
2025-09-18 18:53:03 +0530

    Update calc.r

--- calc.r
+++ calc.r
@@ -1,32 +1,15 @@
-add <- function(x, y) {
-  return(x + y)
-}
-
-subtract <- function(x, y) {
-  return(x - y)
-}
-
-multiply <- function(x, y) {
-  return(x * y)
-}
-
-divide <- function(x, y) {
-  return(x / y)
-}
-
-# take input from the user
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-choice = as.integer(readline(prompt="Enter choice[1/2/3/4]: "))
-
-num1 = as.integer(readline(prompt="Enter first number: "))
-num2 = as.integer(readline(prompt="Enter second number: "))
-
-operator <- switch(choice,"+","-","*","/")
-result <- switch(choice, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2))
-
-print(paste(num1, operator, num2, "=", result))
+# Calculator script 
+calculator <- function(a, b) 
+{ result <- list( 
+Addition = a + b, 
+Subtraction = a - b, 
+Multiplication = a * b, 
+Division = a / b 
+) 
+return(result) 
+} 
+res <- calculator(20, 4) 
+print(res) 
+saveRDS(res, file = "calculator_output.rds") 
+read_res <- readRDS("calculator_output.rds") 
+print(read_res)
